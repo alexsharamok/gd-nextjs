c370e1afc4dd4dd4694747d97ac888733784103c
alex sharamok <alexanders@uniform.dev>
2024-02-19 11:20:19 +0200

    getServerSide props

--- pages/[[...slug]].tsx
+++ pages/[[...slug]].tsx
@@ -1,5 +1,7 @@
 import React from "react";
-import { createConsoleLogger, getNextPageProps, NextPageProps, PageComponent, Placeholder, UniformContextProvider } from "@uniformdev/next";
+import { createConsoleLogger, getNextConfig, getNextPageProps, NextPageProps, PageComponent, Placeholder, UniformContextProvider } from "@uniformdev/next";
+import { getPageProps } from "@uniformdev/common-client";
+
 
 // Register React components here if you migrate from MVC to React
 const componentsIndex: any = {};
@@ -18,35 +20,64 @@ const Page = function (props: NextPageProps) {
 };
 
 // Using Automatic Static Optimization
-export async function getStaticPaths() {
-  // await import is essential here because getStaticPaths only server-side 
-  // and import must only happen server side (on client-side the code will blow up)
-  const { getStaticPaths } = await import("@uniformdev/next-server");
+// export async function getStaticPaths() {
+//   // await import is essential here because getStaticPaths only server-side 
+//   // and import must only happen server side (on client-side the code will blow up)
+//   const { getStaticPaths } = await import("@uniformdev/next-server");
 
-  if (process.env.UNIFORM_BUILD_MODE !== 'ssr') {
-    // If we are exporting the site directly or via a publish
-    // specify all static paths and let nextjs handle 404
-    return {
-      paths: await getStaticPaths(),
-      fallback: false,
-    };
-  } else {
-    // If we are running the site in dynamic preview then
-    // handle all paths and render them on the fly
-    return {
-      paths: [],
-      fallback: "blocking",
-    };
-  }
-}
+//   if (process.env.UNIFORM_BUILD_MODE !== 'ssr') {
+//     // If we are exporting the site directly or via a publish
+//     // specify all static paths and let nextjs handle 404
+//     return {
+//       paths: await getStaticPaths(),
+//       fallback: false,
+//     };
+//   } else {
+//     // If we are running the site in dynamic preview then
+//     // handle all paths and render them on the fly
+//     return {
+//       paths: [],
+//       fallback: "blocking",
+//     };
+//   }
+// }
 
-export async function getStaticProps({ params }) {
+// export async function getStaticProps({ params }) {
+//   const { tryFindItemId, tryGetTimestamp } = await import('@uniformdev/common-server');
+//   const asPath = "/" + (params?.slug?.join("/") || "");
+//   const itemId = await tryFindItemId(asPath);
+//   const timestamp = await tryGetTimestamp(asPath);
+//   const props = await getNextPageProps({ asPath, itemId, timestamp });
+//   return { props: props, revalidate: 10 };
+// }
+
+export async function getServerSideProps({ params }) {
   const { tryFindItemId, tryGetTimestamp } = await import('@uniformdev/common-server');
   const asPath = "/" + (params?.slug?.join("/") || "");
   const itemId = await tryFindItemId(asPath);
   const timestamp = await tryGetTimestamp(asPath);
   const props = await getNextPageProps({ asPath, itemId, timestamp });
-  return { props: props, revalidate: 10 };
+  return { props: props };
 }
+
+// export async function getStaticProps({ params }) {
+//   const asPath = "/" + (params?.slug?.join("/") || "");
+//     //const props = await getNextPageProps({ asPath });
+//     let config = getNextConfig();
+//     if (preview && process.env.UNIFORM_PREVIEW_API_URL) {
+//       config = {
+//         ...config,
+//         UNIFORM_API_URL: process.env.UNIFORM_PREVIEW_API_URL,
+//       };
+//     }
+//     const props: any = await getPageProps(
+//       fetch,
+//       asPath,
+//       config,
+//       createConsoleLogger()
+//     );
+//   return { props };
+// }
+
 
 export default Page;
